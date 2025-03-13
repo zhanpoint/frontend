47d448680adce2303b459278df6cb07a1511d5ca
zhanpoint <2201609540@qq.com>
2025-03-13 17:08:30 +0800

    开始

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button>点击我</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+
+// 获取当前文件的目录路径
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
